3016927d5fe8f2b0c8e06f4a92b990e7382b3718
Kotaro Sonoda <sonoda@cis.nagasaki-u.ac.jp>
2020-05-07 14:16:21 +0900

    課題1修正指示

--- k01/k01.c
+++ k01/k01.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave);
+extern double ave_online(double val, double ave);
 extern double var_online(double val, double ave, double square_ave);
 
 int N;
@@ -13,60 +13,61 @@ int main(void)
     double val, u, a;
     char fname[FILENAME_MAX];
     char buf[256];
-    FILE* fp;
+    FILE *fp;
     double ave = 0;
     double var = 0;
     double square_ave = 0;
 
     printf("input the filename of sample:");
-    fgets(fname,sizeof(fname),stdin);
-    fname[strlen(fname)-1] = '\0';
-    printf("the filename of sample: %s\n",fname);
+    fgets(fname, sizeof(fname), stdin);
+    fname[strlen(fname) - 1] = '\0';
+    printf("the filename of sample: %s\n", fname);
 
-    fp = fopen(fname,"r");
-    if(fp==NULL){
-        fputs("File open error\n",stderr);
+    fp = fopen(fname, "r");
+    if (fp == NULL)
+    {
+        fputs("File open error\n", stderr);
         exit(EXIT_FAILURE);
     }
 
-N = 0;
+    N = 0;
 
-    while(fgets(buf,sizeof(buf),fp) != NULL){
-        sscanf(buf,"%lf",&val);
+    while (fgets(buf, sizeof(buf), fp) != NULL)
+    {
+        sscanf(buf, "%lf", &val);
 
-    N = N + 1;
+        N = N + 1;
 
-    var = var_online(val, ave, square_ave);
-    
-    square_ave = ave_online(val*val, square_ave);
+        var = var_online(val, ave, square_ave);
 
-    ave = ave_online(val, ave);
+        square_ave = ave_online(val * val, square_ave);
 
-}
+        ave = ave_online(val, ave);
+    }
 
-printf("ave = %lf\n", ave);
-printf("var = %lf\n", var);
+    printf("ave = %lf\n", ave);
+    printf("var = %lf\n", var);
 
-u = N * var / (N-1);
-a = ave;
+    u = N * var / (N - 1);
+    a = ave;
 
-printf("母集団の平均 = %lf\n", a);
-printf("母集団の分散 = %lf\n", u);
+    printf("母集団の平均 = %lf\n", a);
+    printf("母集団の分散 = %lf\n", u);
 
-
-    if(fclose(fp) == EOF){
-        fputs("file close error\n",stderr);
+    if (fclose(fp) == EOF)
+    {
+        fputs("file close error\n", stderr);
         exit(EXIT_FAILURE);
     }
 
     return 0;
 }
 
-double ave_online(double val,double ave)
+double ave_online(double val, double ave)
 {
     double ave2;
 
-    ave2 = (N-1) * ave / N + val / N;
+    ave2 = (N - 1) * ave / N + val / N;
 
     return ave2;
 }
@@ -75,7 +76,7 @@ double var_online(double val, double ave, double square_ave)
 {
     double var;
 
-    var = ((N-1)*square_ave/N+val*val/N) - ((N-1)*ave/N+val/N)*((N-1)*ave/N+val/N);
+    var = ((N - 1) * square_ave / N + val * val / N) - ((N - 1) * ave / N + val / N) * ((N - 1) * ave / N + val / N);
 
     return var;
 }
