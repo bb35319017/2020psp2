d039b557324c6cc9e7d5886d4f0c65e12149f7b1
bb35319017 <bb35319017@ms.nagasaki-u.ac.jp>
2020-07-01 15:46:31 +0900

    7/1#1

--- k03/k03.c
+++ k03/k03.c
@@ -20,9 +20,13 @@ int main(int argc, char* argv[])
     int i;
     double dummy;
 
-
-
-
+if(argc<4)
+{
+    return 1;
+}
+sscanf(argv[1], "%lf", &mu);
+sscanf(argv[2], "%lf", &sigma);
+sscanf(argv[3], "%d", &num_dummy);
 
     printf("============================================\n");
     printf("template mean: %4.1lf\n",mu);
@@ -31,7 +35,7 @@ int main(int argc, char* argv[])
     printf("============================================\n");
 
     srand(RAND_SEED);
-    for(i=1; i<=5; i++)
+    for(i=1; i<=num_dummy; i++)
     {
         /* r_stdnormを使って，1人のデータを捏造 */
         dummy = sigma * r_stdnorm() + mu  ;
